d7871fd3e0a050ee0dacd969eaff6ea74a6e973a
Mathew <mathewkariuki2005@gmail.com>
2025-06-18 01:10:41 +0300

    added data to index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName="bob";
+ function upperCaseCustomerName() {
+    customerName=customerName.toUpperCase();
+    console.log(customerName);
+};
+ function setBestCustomer() {
+     bestCustomer = "not bob";
+    
+    console.log(bestCustomer);
+};
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+    console.log(bestCustomer);    
+};
+const leastFavoriteCustomer="customer";
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer="text";
+};
+// upperCaseCustomerName();
+// setBestCustomer();
+// overwriteBestCustomer();
+// changeLeastFavoriteCustomer();
